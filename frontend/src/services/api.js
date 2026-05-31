588aa622a067809be7bf5cf8926252981a6854ff
ANWAR-26 <ahsan.nour.02@gmail.com>
2026-05-31 11:43:02 +0300

    Production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+export default axios.create({
+  baseURL: "https://YOUR-BACKEND.onrender.com/api/",
 });
-
-export default API;
